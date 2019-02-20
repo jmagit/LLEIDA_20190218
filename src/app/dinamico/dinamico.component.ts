61f59fea00740e1865f35caa0c2538245ea7df71
“Javier“ <jmartin@grupoloyal.com>
2019-02-20 11:54:10 +0100

    calculadora

--- src/app/dinamico/dinamico.component.ts
+++ src/app/dinamico/dinamico.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { DemoComponent } from '../demo/demo.component';
 import { LoggerService } from 'src/indra-core';
+import { CalculadoraComponent } from '../calculadora/calculadora.component';
 
 @Component({
   selector: 'app-dinamico',
   templateUrl: './dinamico.component.html',
   styleUrls: ['./dinamico.component.css'],
-  entryComponents: [ HomeComponent, DemoComponent, ]
+  entryComponents: [ HomeComponent, DemoComponent, CalculadoraComponent, ]
 })
 export class DinamicoComponent implements OnInit {
   menu = [
-    {texto: 'Inicio', componente: HomeComponent },
     {texto: 'Demo', componente: DemoComponent },
+    {texto: 'Calculadora', componente: CalculadoraComponent },
+    {texto: 'Inicio', componente: HomeComponent },
   ];
   seleccionado = this.menu[0].componente;
 
